8c5570aa7a8727735a3d8eee1e4990b586cc7d7b
code3743 <jota.lopez@correounivalle.edu.co>
2024-04-08 11:31:21 -0500

    Agregar ruta de búsqueda de noticias

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { getAllNewsController, getNewsDetailsController } = require('../controllers/news.controller');
+const { searchNewsController } = require('../controllers/search.controller');
 
 const router = Router();
 
 router.get('/noticias', getAllNewsController);
 router.get('/noticia/:slug', getNewsDetailsController);
+router.get('/buscar', searchNewsController );
 
 module.exports = {
     router
