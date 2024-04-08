f6f294678370762cf028591c578e90b02686b209
code3743 <jota.lopez@correounivalle.edu.co>
2024-04-08 11:30:51 -0500

    Agregar servicio de búsqueda y raspado de noticias

---
+++ src/services/search.scraper.service.js
@@ -0,0 +1,31 @@
+const cheerio = require('cheerio');
+
+/**
+ * 
+ * @param {string} html 
+ */
+const searchNewsServices = (html) => {
+    try {
+        const $ = cheerio.load(html);
+        const headlinesRaw = $('.views-field.views-field-title a');
+        const headlines = headlinesRaw.map((_, element) => {
+            return {
+                title: $(element).text().trim(),
+                slug: $(element).attr('href')
+            }
+        }).get();
+        const lastPageRaw = $('.pager .pager-last > a')?.attr('href');
+        const lastPage = lastPageRaw ? lastPageRaw.split('&page=')[1].trim() : 0;
+        return {
+            headlines,
+            lastPage
+        };
+    } catch (error) {
+        console.log(error);
+    }
+} 
+
+
+module.exports = {
+    searchNewsServices
+}
